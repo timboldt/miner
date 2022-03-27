71c249286c148704c0a5cee3159495601352490d
Tim Boldt <tim.boldt@gmail.com>
2022-03-26 21:23:39 -0700

    Simple prototype from example code

--- src/main.rs
+++ src/main.rs
@@ -14,33 +14,119 @@
 
 #![warn(clippy::all, clippy::pedantic)]
 
-mod map;
-mod prelude;
+use bevy::{
+    math::ivec3,
+    prelude::*,
+    render::camera::{ActiveCameras, Camera},
+};
 
-use crate::prelude::*;
+use bevy_simple_tilemap::prelude::*;
 
-struct State {
-    map: Map,
+fn main() {
+    App::new()
+        // Disable MSAA, as it produces weird rendering artifacts
+        .insert_resource(Msaa { samples: 1 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(SimpleTileMapPlugin)
+        .add_startup_system(setup)
+        .add_system(input_system)
+        .run();
 }
 
-impl State {
-    fn new() -> Self {
-        Self { map: Map::new() }
+fn input_system(
+    active_cameras: Res<ActiveCameras>,
+    mut camera_transform_query: Query<(&mut Transform,), With<Camera>>,
+    mut tilemap_visible_query: Query<&mut Visibility, With<TileMap>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    const MOVE_SPEED: f32 = 1000.0;
+    const ZOOM_SPEED: f32 = 10.0;
+
+    if let Some(active_camera_entity) = active_cameras.get("camera_2d").and_then(|ac| ac.entity) {
+        if let Ok((mut tf,)) = camera_transform_query.get_mut(active_camera_entity) {
+            if keyboard_input.pressed(KeyCode::X) {
+                tf.scale -= Vec3::splat(ZOOM_SPEED) * time.delta_seconds();
+            } else if keyboard_input.pressed(KeyCode::Z) {
+                tf.scale += Vec3::splat(ZOOM_SPEED) * time.delta_seconds();
+            }
+
+            if keyboard_input.pressed(KeyCode::A) {
+                tf.translation.x -= MOVE_SPEED * time.delta_seconds();
+            } else if keyboard_input.pressed(KeyCode::D) {
+                tf.translation.x += MOVE_SPEED * time.delta_seconds();
+            }
+
+            if keyboard_input.pressed(KeyCode::S) {
+                tf.translation.y -= MOVE_SPEED * time.delta_seconds();
+            } else if keyboard_input.pressed(KeyCode::W) {
+                tf.translation.y += MOVE_SPEED * time.delta_seconds();
+            }
+
+            if keyboard_input.just_pressed(KeyCode::V) {
+                // Toggle visibility
+                let mut visible = tilemap_visible_query.iter_mut().next().unwrap();
+                visible.is_visible = !visible.is_visible;
+            }
+        }
     }
 }
 
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        self.map.render(ctx);
-    }
-}
+fn setup(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    // Load tilesheet texture and make a texture atlas from it
+    let texture_handle = asset_server.load("64x64_tileset.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 14, 14);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
-        .with_title("Miner")
-        .with_fps_cap(30.0) // (7)
-        .build()?;
+    let tiles = vec![
+        (
+            ivec3(-1, 0, 0),
+            Some(Tile {
+                sprite_index: 14,
+                ..Default::default()
+            }),
+        ),
+        (
+            ivec3(1, 0, 0),
+            Some(Tile {
+                sprite_index: 1,
+                ..Default::default()
+            }),
+        ),
+        (
+            ivec3(0, -1, 0),
+            Some(Tile {
+                sprite_index: 2,
+                ..Default::default()
+            }),
+        ),
+        (
+            ivec3(0, 1, 0),
+            Some(Tile {
+                sprite_index: 3,
+                ..Default::default()
+            }),
+        ),
+    ];
 
-    main_loop(context, State::new())
+    let mut tilemap = TileMap::default();
+    tilemap.set_tiles(tiles);
+
+    let tilemap_bundle = TileMapBundle {
+        tilemap,
+        texture_atlas: texture_atlas_handle.clone(),
+        transform: Transform {
+            scale: Vec3::splat(0.75),
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(tilemap_bundle);
 }
